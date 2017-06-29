01873a58333beef8c8b219a571a216c3658983e5
frostfrozen <davidnicolasbogo@hotmail.com>
2017-06-29 11:00:18 -0300

    arreglos varios e variable extern como corte

--- scanner.c
+++ scanner.c
@@ -2,74 +2,101 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include "scanner.h"
-int tt[7][4] = {{2,1,0 ,5},{5, 1, 3, 5},{2, 2, 4, 5},{99, 99, 99, 99},{99, 99, 99, 99},{5, 5, 6, 5},{99,99,99,99}};
+int tt[7][4] = {{2 ,1 ,0 ,5 },
+                {5 ,1 ,3 ,5 },
+                {2 ,2 ,4 ,5 },
+                {99,99,99,99},
+                {99,99,99,99},
+                {6 ,6 ,6 ,5 },
+                {99,99,99,99}};
 /* estados [0][0]
 [1][0] [2][0] numeros, letra
 [3][0] [4][0] aceptores
 [5][0]        error
 */
-char c;
-char parser(FILE *automata)
+char caracter; /* caracter que esta siendo escaneado*/
+enum tipo {LETRA, NUMERO, ESPACIOS, ERROR }; /*columnas*/
+extern int esFinal = 0; /* variable que chequea cuando debe parar*/
+
+char scanner(FILE *automata)
+{
+    int estado = 0;
+    /*funcion scanner*/
+    while (!debo_parar(estado, automata))
     {
-        /*variables*/
-        int estado = 0;
-        int caracter;
-        enum tipo{LETRA, NUMERO, ESPACIOS, ERROR };
-        /*variables*/
-
-        /*funcion parser*/
-       while (!debo_parar(estado, automata))
+        caracter = getc(automata);
+        if (caracter != EOF)
         {
-        c = getc(automata);
-        if (c != EOF)
-            {
-                if (esLetra(c))
-                    {
-                        caracter = LETRA;
-                    }
-                else if (esNumero(c))
-                    {
-                        caracter = NUMERO;
-                    }
-                else if (esEspacio(c))
-                    {
-                        caracter = ESPACIOS;
-                    }
-                else
-                    {
-                        caracter = ERROR;
-                    }
-                estado = tt[estado][caracter];
-            }
-        else {estado = tt[estado][ESPACIOS];}
-        };
-        return estado;
-        };
-        /*funcion parser*/
+            estado = tt[estado][obtener_columna(caracter)];
+        }
+        else
+        {
+            estado = tt[estado][ESPACIOS];
+            esFinal = 1;
+        }
+    };
+    return estado;
+};
+/*funcion scanner*/
 
- /*aceptor / centinela*/
+/* funcion para obtener la columna*/
+int obtener_columna (char caracter)
+{
+    int tipoCaracter;
+
+     if (esLetra(caracter))
+            {
+                tipoCaracter = LETRA;
+            }
+            else if (esNumero(caracter))
+            {
+                tipoCaracter = NUMERO;
+            }
+            else if (esEspacio(caracter))
+            {
+                tipoCaracter = ESPACIOS;
+            }
+            else
+            {
+                tipoCaracter = ERROR;
+            }
+            return tipoCaracter;
+}
+/* funcion para obtener la columna*/
+
+/*aceptor / centinela*/
 int debo_parar(int estado, FILE *automata)
 {
-    if (estado == 3 || estado == 4){
+    if (estado == 3 || estado == 4)
+    {
         if (estado == 99)
         {
-            ungetc(c, automata);
+            ungetc(caracter, automata);
         }
         return 1;
     }
     else
-        {
+    {
         if (estado == 6)
-                return 1;
+            return 1;
         return 0;
-        }
+    }
 }
 /*aceptor / centinela*/
 
 /*auxiliares no necesarias pero hacen el codigo mas legible en español*/
-int esLetra(char l){return islower(l);}
-int esNumero(char l){return isdigit(l);}
-int esEspacio(char l){return isspace(l);};
+int esLetra(char l)
+{
+    return islower(l);
+}
+int esNumero(char l)
+{
+    return isdigit(l);
+}
+int esEspacio(char l)
+{
+    return isspace(l);
+};
 /*auxiliares no necesarias pero hacen el codigo mas legible en español*/
 
 
--- scanner.h
+++ scanner.h
@@ -1,11 +1,13 @@
 #ifndef SCANNER_H_INCLUDED
 #define SCANNER_H_INCLUDED
 /*funciones*/
-char parser(FILE *automata); //funcion principal Automata Finito Deterministico
+extern int esFinal;
+char scanner(FILE *automata); //funcion principal Automata Finito Deterministico
 int debo_parar(int estado, FILE *automata);
 int esLetra(char l);
 int esNumero(char l);
 int esEspacio(char l);
+int obtener_columna (char caracter);
 /*funciones*/
 #endif // SCANNER_H_INCLUDED
 
